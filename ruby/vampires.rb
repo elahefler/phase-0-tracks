40d30ff3b59cbd46635613c917a2da4b07af0d2a
Ela Hefler <eleanor@gmail.comEla>
2017-02-16 16:42:43 -0500

    Edit data questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,16 +1,14 @@
-puts "What is your name?"
-name = gets.chomp
 puts "What year were you born?"
 year = gets.chomp
 age = 2017 - year.to_i
-puts "Are you #{age} years old (y/n)?"
-age_y = gets.chomp
-	if age_y == "y" 
-		puts "You are #{age} years old"
-	else age_y =="n"
-		age -= 1
-	end
 puts "Do you want garlic bread (y/n)?"
 bread = gets.chomp
 puts "Would you like to enroll in the Company's health insurance?"
 health = gets.chomp
+puts "What is your name?"
+name = gets.chomp
+	if name == "Drake Cula" || name == "Tu Fang"
+		puts "Definitely a vampire"
+	else
+		puts "Results inconclusive"
+	end
