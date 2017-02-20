408f763f3b286a5d388957417e5fbfb1a98d63af
Ela Hefler <eleanor@gmail.comEla>
2017-02-20 01:09:20 -0500

    Correct typos

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -11,27 +11,26 @@ while $count <= employees
 	age = 2017 - year.to_i
 	puts "Do you want garlic bread (y/n)?"
 	bread = gets.chomp
-	puts "Would you like to enroll in the Company's health insurance?"
+	puts "Would you like to enroll in the Company's health insurance? (y/n)"
 	health = gets.chomp
 	puts "What is your name?"
 	name = gets.chomp
-  puts "Please name your alergies one at a time. Type 'done' when finished"
-	$alergies = gets.chomp
-    until $alergies == "sunshine" || $alergies == "done"
-    	puts "Do you have any other alergies?"
-    	puts "Please name your alergies one at a time. Type 'done' when finished"
-    	$alergies = gets.chomp
+  puts "Please name your allergies one at a time. Type 'done' when finished"
+	$allergies = gets.chomp
+    until $allergies == "sunshine" || $allergies == "done"
+    	puts "Do you have any other allergies?"
+    	puts "Please name your allergies one at a time. Type 'done' when finished"
+    	$allergies = gets.chomp
     end
-	if name == "Drake Cula" || name == "Tu Fang" || $alergies == "sunshine"
+	if name == "Drake Cula" || name == "Tu Fang" || $allergies == "sunshine"
 		puts "Definitely a vampire"
-	elsif $alergies == "sunshine"
+	elsif $allergies == "sunshine"
 		puts "Definitely a vampire" 
 	elsif age < 100 && (bread == "y" || health == "y")
 		puts "Probably not a vampire"
 	elsif (age > 101 && (bread == "n" && health == "n"))
 		puts "Almost certainly a vampire"
 	elsif (age > 101 && (bread == "n" || health == "n"))
-		next if $alergies == "sunshine"
 		puts "Probably a vampire"
 	else
 		puts "Results inconclusive"
