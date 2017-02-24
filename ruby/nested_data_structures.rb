631fcb5611c66d0a04ff5a668b930622c26662c4
Ela Hefler <eleanor@gmail.comEla>
2017-02-24 01:43:18 -0500

    build nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,1 +1,35 @@
- 
+meal_ingredients = {
+	salad: {
+		lettuce: "romaine",
+		dressing: "ranch",
+		protien: {
+			meat: "chicken"
+			cheese: "parmesan"
+		},
+		vegetables: [
+			"peppers",
+			"tomatoes",
+			"cucumbers"
+		]
+	},
+	sandwich: {
+		bread: "rye"
+		protien: {
+			meat: "ham"
+			cheese: "cheddar"
+		},
+		vegitables: [
+			"tomato",
+			"onion"
+		]
+		speads: [
+			"mayo",
+			"mustard",
+		]
+	},
+	drink: "milk",
+	dessert: [
+		"icecream",
+		"pie"]
+	}
+}
