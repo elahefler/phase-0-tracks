f9d17fec5b2f9313fd794b25f384a3df02d07f3d
Ela Hefler <eleanor@gmail.comEla>
2017-02-24 02:14:34 -0500

    Add pseudocode for fake name converter

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,7 @@
+#Ask for spy's real name, store in variable
+
+#Split first name and last name
+
+#Reverse order of first name and last name
+
+#loop and change each letter to next consonant or vowel
