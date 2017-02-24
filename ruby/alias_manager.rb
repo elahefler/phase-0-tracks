869499700734dbc064a53c3702352505d3120e2b
Ela Hefler <eleanor@gmail.comEla>
2017-02-24 02:54:13 -0500

    Add reference arrays for consonant and vowel conversion

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,20 +1,37 @@
+#set up hash structure
 spy_name = {
   first_name: "x",
   last_name: "y"
 }
+
+reference = {
+  vowels: "a",
+  consonants: "b"
+}
+
+
+# use case statement to convert consonants and vowels
+reference[:vowels] = "aeiou".split('')
+p reference[:vowels]
+reference[:consonants] = "bcdfghjklmnpqrstvwxyz".split('')
+p reference[:consonants]
+
 #Ask for spy's real name, store in variable
 puts "What is your full name"
 full_name = gets.chomp
 
-#Split first name and last name
+#convert uppercase letters to lowercase
 lowercase_name = full_name.downcase
+
+#Split first name and last name
 split_name = lowercase_name.split(' ')
 p split_name
+
 #Reverse order of first name and last name
 reverse_name = split_name.reverse
 p reverse_name
 
-#loop and change each letter to next consonant or vowel
+#store letters of each name as arrays in spy_name hash
 first_name = reverse_name[0].split('')
 p first_name
 spy_name[:first_name] = first_name
