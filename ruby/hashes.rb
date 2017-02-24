0e75665b575c4fa61e33db30c08d920856c0d12a
Ela Hefler <eleanor@gmail.comEla>
2017-02-24 01:06:02 -0500

    Add case statement to modify client data

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,7 +2,7 @@ client_data = {}
 
 # Ask for client's name and save it in a key
 puts "What is your name?"
-client_data[:name].to_sym = gets.chomp
+client_data[:name] = gets.chomp
 
 # Ask for client's age and save it in a key
 puts "How old are you?"
@@ -11,7 +11,6 @@ age = age.to_i
 client_data[:age] = age
 
 # Ask for client's number of children and save it in a key
-puts "How many children do you have?"
 puts "How many children do you have?"
 children = gets.chomp
 children = children.to_i
@@ -44,40 +43,47 @@ p client_data
 puts "Would you like to change any of your answers (please put yes or none)"
 changes = gets.chomp
 
-index = 1
-
-Until index == 0
-	if changes = "yes"
-		puts "What answer would you like to change?"
-		answer_change = gets.chomp
-			if answer_change = "name"
-				puts "What is your new answer?"
-				new_answer = gets.chomp
-			elsif answer_change = "age"
-				puts "What is your new answer?"
-				new_answer = gets.chomp
-				new_answer = new_answer.to_i
-			elsif answer_change = "children"
-				puts "What is your new answer?"
-				new_answer = gets.chomp
-				new_answer = new_answer.to_i
-			elsif answer_change = "decor"
-				puts "What is your new answer?"
-				new_answer = gets.chomp
-			elsif answer_change = "payment"
-				puts "What is your new answer?"
-					if new_answer = "y"
-						new_answer = true
-				new_answer = gets.chomp
-			elsif answer_change = "home"
-				puts "What is your new answer?"
-				new_answer = gets.chomp
+#define case statement for for answer change options
+def answer_change
+if changes = "yes"
+	puts "What answer would you like to change?"
+	answer_change = gets.chomp
+		if answer_change == "name"
+			puts "What is your new answer?"
+			new_answer = gets.chomp
+			client_data[:name] == new_answer
+		elsif answer_change == "age"
+			puts "What is your new answer?"
+			new_answer = gets.chomp
+			new_answer = new_answer.to_i
+			client_data[:age] = new_answer
+		elsif answer_change == "children"
+			puts "What is your new answer?"
+			new_answer = gets.chomp
+			new_answer = new_answer.to_i
+			client_data[:children] = new_answer
+		elsif answer_change == "decor"
+			puts "What is your new answer?"
+			new_answer = gets.chomp
+			client_data[:decor] = new_answer
+		elsif answer_change == "payment"
+			puts "What is your new answer?"
+			new_answer = gets.chomp
 				if new_answer = "y"
-						new_answer = true
+					new_answer = true
+			  end
+			 client_data[:payment] = new_answer
+		elsif answer_change == "home"
+			puts "What is your new answer?"
+			new_answer = gets.chomp
+			if new_answer = "y"
+					new_answer = true
+			client_data[:home] = new_answer
+		  end
+		end
+end
+end
 
-
-
-
-
-
-
+#print responses and end program
+p "Here are your responses #{client_data}"
+puts "Thank you. Bye"
