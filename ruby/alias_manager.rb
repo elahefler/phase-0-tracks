8ddf3234d655a625b1334e19d164ea86b41980d4
Ela Hefler <eleanor@gmail.comEla>
2017-02-27 01:21:28 -0500

    Add loop

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,159 +1,147 @@
-#set up hash structure
-#! spy_name = {
-#!  first_name: "x",
-#!  last_name: "y"
-# }
+loop do 
+  #Ask for spy's real name, store in variable
+  puts "Enter a name to generate an alias"
+  full_name = gets.chomp
+    break if full_name == "quit"
+  
+  #convert uppercase letters to lowercase
+  lowercase_name = full_name.downcase
+  
+  #Split first name and last name
+  split_name = lowercase_name.split(' ')
+  ##split_name
+  
+  #Reverse order of first name and last name
+  reverse_name = split_name.reverse
+  ##reverse_name
+  
+  #store letters of each name as arrays in spy_name hash
+  alias_hash = Hash.new {|hash, key| hash[key] = []}
+  
+  alias_hash["New First Name"] = reverse_name[0].split('')
+  alias_hash["New Last Name"] = reverse_name[1].split('')
+  
+  ##p alias_hash
+  
+  # Replace each value of "New First Name"
+  alias_hash["New First Name"].map! { |letter|
+     if (letter == "a")
+         "e" 
+     elsif (letter == "e")
+         "i" 
+      elsif (letter == "i")
+         "o"     
+      elsif (letter == "o")
+         "u"
+      elsif (letter == "u")
+         "a"
+      elsif (letter == "b")
+         "c" 
+      elsif (letter == "c")
+         "d"     
+      elsif (letter == "d")
+         "f"
+      elsif (letter == "f")
+         "g"
+       elsif (letter == "g")
+         "h" 
+      elsif (letter == "h")
+         "j"     
+      elsif (letter == "j")
+         "k"
+      elsif (letter == "k")
+         "l"
+       elsif (letter == "l")
+         "m" 
+      elsif (letter == "m")
+         "n"     
+      elsif (letter == "n")
+         "p"
+      elsif (letter == "p")
+         "q"
+      elsif (letter == "q")
+         "r" 
+      elsif (letter == "r")
+         "s"     
+      elsif (letter == "s")
+         "t"
+      elsif (letter == "t")
+         "v"
+      elsif (letter == "v")
+         "w" 
+      elsif (letter == "w")
+         "x"     
+      elsif (letter == "x")
+         "y"
+      elsif (letter == "y")
+         "z"
+      elsif (letter == "z")
+         "b"
+     end
+  }
+  
+  # Replace each value of "New Last Name"
+  alias_hash["New Last Name"].map! { |letter|
+     if (letter == "a")
+         "e" 
+     elsif (letter == "e")
+         "i" 
+      elsif (letter == "i")
+         "o"     
+      elsif (letter == "o")
+         "u"
+      elsif (letter == "u")
+         "a"
+      elsif (letter == "b")
+         "c" 
+      elsif (letter == "c")
+         "d"     
+      elsif (letter == "d")
+         "f"
+      elsif (letter == "f")
+         "g"
+       elsif (letter == "g")
+         "h" 
+      elsif (letter == "h")
+         "j"     
+      elsif (letter == "j")
+         "k"
+      elsif (letter == "k")
+         "l"
+       elsif (letter == "l")
+         "m" 
+      elsif (letter == "m")
+         "n"     
+      elsif (letter == "n")
+         "p"
+      elsif (letter == "p")
+         "q"
+      elsif (letter == "q")
+         "r" 
+      elsif (letter == "r")
+         "s"     
+      elsif (letter == "s")
+         "t"
+      elsif (letter == "t")
+         "v"
+      elsif (letter == "v")
+         "w" 
+      elsif (letter == "w")
+         "x"     
+      elsif (letter == "x")
+         "y"
+      elsif (letter == "y")
+         "z"
+      elsif (letter == "z")
+         "b"
+     end
+  }
+  ## p alias_hash
+alias_first = alias_hash["New First Name"]*''
+alias_first = alias_first.capitalize!
+  
+alias_last = alias_hash["New Last Name"]*''
+alias_last = alias_last.capitalize!
 
-reference = {
-  vowels: "a",
-  consonants: "b"
-}
-
-
-#Ask for spy's real name, store in variable
-puts "What is your full name"
-full_name = gets.chomp
-
-#convert uppercase letters to lowercase
-lowercase_name = full_name.downcase
-
-#Split first name and last name
-split_name = lowercase_name.split(' ')
-p split_name
-
-#Reverse order of first name and last name
-reverse_name = split_name.reverse
-p reverse_name
-
-#store letters of each name as arrays in spy_name hash
-alias_hash = Hash.new {|hash, key| hash[key] = []}
-
-alias_hash["New First Name"] = reverse_name[0].split('')
-alias_hash["New Last Name"] = reverse_name[1].split('')
-
-p alias_hash
-
-# Replace each value of "New First Name"
-alias_hash["New First Name"].map! { |letter|
-   if (letter == "a")
-       "e" 
-   elsif (letter == "e")
-       "i" 
-    elsif (letter == "i")
-       "o"     
-    elsif (letter == "o")
-       "u"
-    elsif (letter == "u")
-       "a"
-    elsif (letter == "b")
-       "c" 
-    elsif (letter == "c")
-       "d"     
-    elsif (letter == "d")
-       "f"
-    elsif (letter == "f")
-       "g"
-     elsif (letter == "g")
-       "h" 
-    elsif (letter == "h")
-       "j"     
-    elsif (letter == "j")
-       "k"
-    elsif (letter == "k")
-       "l"
-     elsif (letter == "l")
-       "m" 
-    elsif (letter == "m")
-       "n"     
-    elsif (letter == "n")
-       "p"
-    elsif (letter == "p")
-       "q"
-    elsif (letter == "q")
-       "r" 
-    elsif (letter == "r")
-       "s"     
-    elsif (letter == "s")
-       "t"
-    elsif (letter == "t")
-       "v"
-    elsif (letter == "v")
-       "w" 
-    elsif (letter == "w")
-       "x"     
-    elsif (letter == "x")
-       "y"
-    elsif (letter == "y")
-       "z"
-    elsif (letter == "z")
-       "b"
-   end
-}
-
-# Replace each value of "New Last Name"
-alias_hash["New Last Name"].map! { |letter|
-   if (letter == "a")
-       "e" 
-   elsif (letter == "e")
-       "i" 
-    elsif (letter == "i")
-       "o"     
-    elsif (letter == "o")
-       "u"
-    elsif (letter == "u")
-       "a"
-    elsif (letter == "b")
-       "c" 
-    elsif (letter == "c")
-       "d"     
-    elsif (letter == "d")
-       "f"
-    elsif (letter == "f")
-       "g"
-     elsif (letter == "g")
-       "h" 
-    elsif (letter == "h")
-       "j"     
-    elsif (letter == "j")
-       "k"
-    elsif (letter == "k")
-       "l"
-     elsif (letter == "l")
-       "m" 
-    elsif (letter == "m")
-       "n"     
-    elsif (letter == "n")
-       "p"
-    elsif (letter == "p")
-       "q"
-    elsif (letter == "q")
-       "r" 
-    elsif (letter == "r")
-       "s"     
-    elsif (letter == "s")
-       "t"
-    elsif (letter == "t")
-       "v"
-    elsif (letter == "v")
-       "w" 
-    elsif (letter == "w")
-       "x"     
-    elsif (letter == "x")
-       "y"
-    elsif (letter == "y")
-       "z"
-    elsif (letter == "z")
-       "b"
-   end
-}
-p alias_hash
-
-
-
-
-
-	
-
-
-
-
+p "Your alias is #{alias_first} #{alias_last}"
+end 
