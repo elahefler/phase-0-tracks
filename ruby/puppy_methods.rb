4ea5a3b237e936cb025e105948fbb618a61f4ca8
Ela Hefler <eleanor@gmail.comEla>
2017-02-28 17:11:01 -0500

    Add puppy methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,31 @@
 class Puppy
 
+	def initalize
+		puts "Initializing puppy method"
+	end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
-end
+  def speak(num)
+  	num.times do puts "Woof!"
+  		end
+  	num
+  end
 
+ def dog_years(year)
+  	puts year*7
+  	year
+  end
+
+
+puppy = Puppy.new
+puppy.fetch ("ball")
+
+puppy.speak (2)
+
+puppy.dog_years(3)
+
+end
